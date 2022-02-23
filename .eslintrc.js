ddce55c787584b7171d97707cd7a8666fd84a81a
Jake Schmitz <jschmitz@gmail.com>
2022-02-23 08:44:19 -0600

    Fix eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'jsx-a11y',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+};
