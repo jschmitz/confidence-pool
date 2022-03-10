673f5cf59a863bf5b7925c80d47874de05876654
Jake Schmitz <jschmitz@gmail.com>
2022-03-10 06:38:24 -0600

    Add temp button to  udpate mash state

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import mash from "./mash.json";
 import Cell from "./Cell";
 
 function App() {
+  const [stateMash, setStateMash] = useState(mash);
+
   const headers = mash[0].headers || [{ id: "id", label: "label" }];
   const rows = mash[1].rows || [{ id: "id", label: "label" }];
-  const footers = mash[mash.length - 1].footers || [{ id: "id", label: [] }];
+  const footers = stateMash[mash.length - 1].footers || [
+    { id: "id", label: [] }
+  ];
+
+  const buttonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setStateMash(setFooter(JSON.stringify(stateMash), 2, "1,000,000"));
+  };
+
+  function setFooter(mashCopy: string, index: number, newValue: string) {
+    const clonedMash = JSON.parse(mashCopy);
+    const f = clonedMash[2]["footers"].find(
+      (total: any) => total.id == `footer${index}`
+    );
+    f.label = newValue;
+    return clonedMash;
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Confidence Pool</h1>
+        <button type="button" onClick={buttonHandler}>
+          Update Player 1 FTW
+        </button>
         <table>
           <thead>
             <tr>
