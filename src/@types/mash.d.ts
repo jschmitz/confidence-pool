964fec2281e21ca071839c3c77496c4ee9a7128c
Jake Schmitz <jschmitz@gmail.com>
2022-03-23 08:31:21 -0500

    Add type definitions file for mash

---
+++ src/@types/mash.d.ts
@@ -0,0 +1,52 @@
+// mash.ts
+export interface IMash {
+  header_cells: IHeader[];
+  body_rows: IBodyRow[];
+  footer_cells: IFooter[];
+}
+
+export interface IHeader {
+  id: string;
+  label: string;
+}
+
+export interface IGameCell {
+  id: string;
+  game_name: string;
+  game_id: number;
+  cell_type: string;
+}
+
+export interface IGameResultCell {
+  id: string;
+  game_result: boolean;
+  game_id: number;
+  cell_type: string;
+}
+
+export interface IPickCell {
+  id: string;
+  game_id: number;
+  player_id: number;
+  pick_id: number;
+  pick_result: boolean;
+  pick_value: number;
+  cell_type: string;
+  game_result: boolean;
+}
+
+export type TCell = IGameCell | IGameResultCell | IPickCell;
+
+export interface IBodyRow {
+  id: string;
+  cells: TCell[];
+}
+
+export interface IFooter {
+  id: string;
+  label: string;
+}
+
+export type Props = {
+  children: ReactNode;
+};
--- src/App.tsx
+++ src/App.tsx
@@ -3,54 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import mash from "./mash.json";
 import Cell from "./Cell";
-
-export interface IMash {
-    header_cells: IHeader[],
-    body_rows: IBodyRow[],
-    footer_cells: IFooter[]
-}
-
-interface IHeader {
-    id: string,
-    label: string
-}
-
-interface IGameCell {
-    id: string,
-    game_name: string,
-    game_id: number,
-    cell_type: string
-}
-
-interface IGameResultCell {
-    id: string,
-    game_result: boolean,
-    game_id: number,
-    cell_type: string
-}
-
-interface IPickCell {
-    id: string,
-    game_id: number,
-    player_id: number,
-    pick_id: number,
-    pick_result: boolean,
-    pick_value: number,
-    cell_type: string,
-    game_result: boolean
-}
-
-type Cell = IGameCell | IGameResultCell | IPickCell
-
-interface IBodyRow {
-    id: string,
-    cells:  Cell[]
-}
-
-interface IFooter {
-    id: string,
-    label: string
-}
+import { IMash, IBodyRow, IFooter, IHeader, TCell } from "./@types/mash";
 
 function App() {
   const [stateMash, setStateMash] = useState(mash);
@@ -71,7 +24,7 @@ function App() {
     const totals = Array(numPlayers).fill(0);
 
     clonedMash.body_rows.forEach((row: IBodyRow) => {
-      row.cells.forEach((cell: Cell, index: number) => {
+      row.cells.forEach((cell: TCell, index: number) => {
         if ("game_result" in cell && "pick_result" in cell){
           if (cell.game_result === cell.pick_result) {
             totals[index - 2] += cell.pick_value;
